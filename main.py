8fde296ee4ef4f9b00e0d89423e637a514bb9501
thayso <thayso.weslley@gmail.com>
2025-08-26 23:43:09 -0300

    atualizado dia 26 de agosto

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+''' Incrementar as funções de Criar usuário e Vincular a conta'''
+
 saldo = 0
 limite = 500
 extrato = ""
@@ -10,6 +12,7 @@ cores = {
     "amarelo": "\033[33m",
     "azul": "\033[36m",
 }
+
 
 def limpar():
     import os
@@ -26,44 +29,54 @@ while True:
 
     opcao = input("\nEscolha uma opção: ")
 
-    if opcao == "1":
-        valor = float(input("Digite o valor a ser depositado: "))
-        limpar()
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: {cores['verde']}R$ {valor:.2f}{cores['limpar']}\n"
-            print(f"Depósito de {cores['verde']}R$ {valor:.2f}{cores['limpar']} realizado com sucesso!")
-        else:
-            print(f"{cores['amarelo']}Valor inválido para depósito.{cores['limpar']}")
+    # Depósito =================================================================================================================================================
 
-    elif opcao == "2":
-        limpar()
-        if numero_saques < LIMITE_SAQUES:
-            valor = float(input("Digite o valor a ser sacado: "))
-            if 0 < valor <= saldo and valor <= limite:
-                saldo -= valor
-                extrato += f"Saque: {cores['azul']}R$ {valor:.2f}{cores['limpar']}\n"
-                numero_saques += 1
-                print(f"Saque de {cores['azul']}R$ {valor:.2f}{cores['limpar']} realizado com sucesso!")
+    match opcao:
+        case "1":
+            valor = float(input("Digite o valor a ser depositado: "))
+            limpar()
+            if valor > 0:
+                saldo += valor
+                extrato += f"Depósito: {cores['verde']}\tR$ {valor:.2f}{cores['limpar']}\n"
+                print(f"Depósito de {cores['verde']}R$ {valor:.2f}{cores['limpar']} realizado com sucesso!")
             else:
-                print(f"{cores['amarelo']}Valor inválido para saque.{cores['limpar']}")
-        else:
-            print(f"{cores['amarelo']}Limite de saques diários atingido.{cores['limpar']}")
+                print(f"{cores['amarelo']}Valor inválido para depósito.{cores['limpar']}")
 
-    elif opcao == "3":
-        limpar()
-        print("Extrato ===========================")
-        if extrato:
-            print(extrato)
-        else:
-            print(f"{cores['amarelo']}Nenhuma transação realizada.{cores['limpar']}")
-        print(f"Saldo atual: {cores['verde']}R$ {saldo:.2f}\n{cores['limpar']}")
+    # Saque ====================================================================================================================================================
 
-    elif opcao == "4":
+        case "2":
+            limpar()
+            if numero_saques < LIMITE_SAQUES:
+                valor = float(input("Digite o valor a ser sacado: "))
+                if 0 < valor <= saldo and valor <= limite:
+                    saldo -= valor
+                    extrato += f"Saque: {cores['azul']}\t\t1R$ {valor:.2f}{cores['limpar']}\n"
+                    numero_saques += 1
+                    print(f"Saque de {cores['azul']}R$ {valor:.2f}{cores['limpar']} realizado com sucesso!")
+                else:
+                    print(f"{cores['amarelo']}Valor inválido para saque.{cores['limpar']}")
+            else:
+                print(f"{cores['amarelo']}Limite de saques diários atingido.{cores['limpar']}")
 
-        print(f"{cores['verde']}\nObrigado por usar o Banco Python! Até logo!\n{cores['limpar']}")
-        break
+    # Extrato ==================================================================================================================================================
 
-    else:
-        limpar()
-        print(f"{cores['amarelo']}Opção inválida, tente novamente.{cores['limpar']}")
+        case "3":
+            limpar()
+            print("Extrato ===========================\n")
+            if extrato:
+                print(extrato)
+            else:
+                print(f"{cores['amarelo']}Nenhuma transação realizada.{cores['limpar']}")
+            print(f"Saldo atual: {cores['verde']}\tR$ {saldo:.2f}\n{cores['limpar']}")
+
+    # Sair ====================================================================================================================================================
+
+        case "4":
+            print(f"{cores['verde']}\nObrigado por usar o Banco Python! Até logo!\n{cores['limpar']}")
+            break
+
+    # Opção inválida ===========================================================================================================================================
+
+        case _:
+            limpar()
+            print(f"{cores['amarelo']}Opção inválida, tente novamente.{cores['limpar']}")
